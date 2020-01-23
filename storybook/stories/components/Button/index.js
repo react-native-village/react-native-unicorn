8d4c6e899a918bd4d6e576ff70369880aa32998f
Dimka Vasilyev <raoffonom@icloud.com>
2020-01-23 20:50:08 +0300

    first commit

---
+++ storybook/stories/components/Button/index.js
@@ -0,0 +1,33 @@
+// @flow
+import React, { memo, useState } from 'react'
+import { StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import type { TextStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet'
+import { BLUE, PINK } from '../constants'
+
+const styles = StyleSheet.create({
+  img: {
+    alignSelf: 'center',
+    fontSize: 40
+    //fontFamily: 'IBM-Logo'
+  }
+})
+
+type ButtonT = {
+  color: string,
+  title: string,
+  onPress: Function,
+  textStyle: TextStyleProp
+}
+
+const Button = memo<ButtonT>(({ color = BLUE, title, onPress, textStyle }) => {
+  const [bg, setBg] = useState(color)
+  const { img } = styles
+
+  return (
+    <TouchableWithoutFeedback onPress={onPress} onPressIn={() => setBg(PINK)} onPressOut={() => setBg(BLUE)}>
+      <Text style={[img, textStyle, { color: bg }]}>{title}</Text>
+    </TouchableWithoutFeedback>
+  )
+})
+
+export { Button }
---
+++ storybook/stories/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {storiesOf} from '@storybook/react-native';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+
+import {Button} from './components';
+import CenterView from './CenterView';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
+
+storiesOf('Button', module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('with text', () => (
+    <Button title="Hello world" onPress={action('clicked-emoji')} />
+  ))
+  .add('with some emoji', () => (
+    <Button title="Hello" onPress={action('clicked-emoji')} />
+  ));
