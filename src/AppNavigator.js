24c600552fbe110eaa1e7345aee062efec222e03
Kurbanov Jasur <41279178+kurbanovjasur@users.noreply.github.com>
2020-05-08 13:15:33 +0500

    navigation updated

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// eslint-disable-next-line import/no-unresolved
+import { createNativeStackNavigator } from 'react-native-screens/native-stack'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { enableScreens } from 'react-native-screens' // eslint-disable-line
 import UIKit from './UIKit'
